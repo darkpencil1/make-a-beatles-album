a7d0109ff04bed4d5605ac0eaf543838b7999618
darkpencil1 <91048012+darkpencil1@users.noreply.github.com>
2022-01-11 14:33:50 +0200

    changes to harrison profile img

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -19,7 +19,7 @@ import SubmitButton from './SubmitButton';
 //images
 import lennon from "./img/lennonprofile.png"
 import mccartney from "./img/mccartney-profile.png"
-import harrison from "../assets/img/harrison-profile.png"
+import harrison from "./img/lennonprofile.png"
 import starr from "./img/starr-profile.png"
 
 
