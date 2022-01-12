690271a821ffdec2ab3b25f4c6c5025796b7ec2c
darkpencil1 <91048012+darkpencil1@users.noreply.github.com>
2022-01-12 09:22:58 +0200

    PageWrapper url based on production/development

--- src/PageWrapper.tsx
+++ src/PageWrapper.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import {Routes, Route} from 'react-router'
+
+import { Path } from 'react-router'
 import Navbar from './components/Navbar'
+
+//Pages
 import AlbumMaker from './Pages/AlbumMaker'
 import Albums from './Pages/Albums'
 import Home from './Pages/Home'
 
+
+let url:string = ""
+//Assign server url based on development mode
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    url = ""
+
+} else {
+   url = window.location.pathname
+}
+console.log("window:", window.location.pathname)
 export default function PageWrapper() {
     return (
         <div>
            <Navbar/>
            <Routes>
-               <Route path="/" element={<Home/>} />
-               <Route path="/maker" element={<AlbumMaker/>} />
-               <Route path="/albums" element={<Albums/>} />
+               <Route path={url.concat("/")} element={<Home/>} />
+               <Route path={url.concat("maker")} element={<AlbumMaker/>} />
+               <Route path={url.concat("albums")} element={<Albums/>} />
             </Routes> 
             
         </div>
