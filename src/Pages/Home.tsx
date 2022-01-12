05fa64303ce272ce75bbfe63056c533f222cb9c8
darkpencil1 <91048012+darkpencil1@users.noreply.github.com>
2022-01-12 09:31:38 +0200

    removed unnecesary files from src/

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import {HashLink} from 'react-router-hash-link'
-import Tracks from '../components/Auth'
 import AlbumMaker from './AlbumMaker'
 import Albums from './Albums'
 
